ad0676d5fd14b10f7c0593cc467f7716913bdfb8
Carlos Cervantes - vn53fns <Carlos.Cervantes1@walmart.com>
2022-04-13 13:14:55 -0500

    13/04/2022 Update Test Case Login v2

--- test/pages/mainPage.js
+++ test/pages/mainPage.js
@@ -6,7 +6,7 @@ module.exports = {
         loginButton: { locateStrategy: 'xpath', selector: '//header/nav[1]/div[1]/ul[2]/li[1]/a[1]'},
         //MAIN PAGE ELEMENTS
         homeBtnApp: {locateStrategy:'xpath', selector:"//body/div[@id='todoist_app']/div[@id='page_background']/div[@id='top_bar']/div[@id='top_bar_inner']/div[1]/button[2]/*[1]"},
-        userOptn: {locateStrategy:'xpath', selector:"/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/button[5]/img[1]"},
+        userOptn: {locateStrategy:'xpath', selector:"//img[@class='user_avatar big settings_avatar']"},
             //OPTIONS USER
             logOut: {locateStrategy:'xpath', selector:"//span[contains(text(),'Log out')]"},
     }
